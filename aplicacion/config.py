46c59976d1ea91c26e7a0adf9012ef6cdbe4ba84
josman55 <infornitalba@gmail.com>
2025-01-30 20:10:57 +0100

    Update config.py

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -6,7 +6,7 @@ PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Asgbd2023$@localhost/flask_db'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:Asgbd2023$@mariadb.crjpvawmtsmq.us-east-1.rds.amazonaws.com/flask_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admintienda:Asgbd2024*@localhost/tienda'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:Asgbd2023$@mariadb.crjpvawmtsmq.us-east-1.rds.amazonaws.com/flask_db'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
